c48ae050311113a94248d585a72b58f61c77c38b
Marcial Soto <hola@marcialsoto.com>
2014-12-17 16:20:33 -0500

    Corrigiendo Menu y Slider

--- wp-content/themes/pssa/lib/extras.php
+++ wp-content/themes/pssa/lib/extras.php
@@ -20,3 +20,33 @@ function roots_wp_title($title) {
   return $title;
 }
 add_filter('wp_title', 'roots_wp_title', 10);
+
+function slide_query($args){
+    // The Query
+  $the_query = new WP_Query( $args );
+
+  // The Loop
+  if ( $the_query->have_posts() ) {
+  	while ( $the_query->have_posts() ) {
+  		$the_query->the_post();
+  		echo '<div class="item">';
+        if ( has_post_thumbnail() ) {
+  	       the_post_thumbnail();
+        }
+
+        echo '<div class="item-caption">';
+          echo '<h1>' . get_the_title() . '</h1>';
+                get_template_part('templates/entry-meta');
+                echo '<p class="item-body">' . get_the_excerpt() . '</p>';
+                echo '<p><a class="btn btn-main" href="' . get_the_permalink() . '">Más Información</a></p>';
+        echo '</div>';
+      echo '</div>';
+  	}
+  } else {
+  	// no posts found
+  }
+  /* Restore original Post Data */
+  wp_reset_postdata();
+}
+
+add_filter('child_slide_query', 'slide_query');
--- wp-content/themes/pssa/templates/slider-home.php
+++ wp-content/themes/pssa/templates/slider-home.php
@@ -1,30 +1,11 @@
 <section id="owl-home" class="owl-carousel owl-theme">
-  <div class="item">
-    <img src="http://lorempixel.com/940/325/nature/1/" alt="The Last of us">
-    <div class="item-caption">
-      <h1>Primer CLAR del Proyecto Sierra y Selva Alta en el distrito de Jepelacio – Moyobamba</h1>
-      <time>4 de Noviembre de 2014</time>
-      <p class="item-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis ut sem sit amet imperdiet</p>
-      <p><a class="btn btn-main" href="#">Más info</a></p>
+  <?php
+    $args = array(
+      'post_type' => 'post',
+      'posts_per_page' => 2,
+      'tag' => 'destacado'
+    );
 
-    </div>
-  </div>
-<div class="item">
-  <img src="http://lorempixel.com/940/325/nature/2/" alt="The Last of us">
-  <div class="item-caption">
-    <h1>Primer CLAR del Proyecto Sierra y Selva Alta en el distrito de Jepelacio – Moyobamba</h1>
-    <time>4 de Noviembre de 2014</time>
-    <p class="item-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis ut sem sit amet imperdiet</p>
-    <p><a class="btn btn-main" href="#">Más info</a></p>
-  </div>
-</div>
-<div class="item">
-  <img src="http://lorempixel.com/940/325/nature/3/" alt="The Last of us">
-  <div class="item-caption">
-    <h1>Primer CLAR del Proyecto Sierra y Selva Alta en el distrito de Jepelacio – Moyobamba</h1>
-    <time>4 de Noviembre de 2014</time>
-    <p class="item-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean mattis ut sem sit amet imperdiet</p>
-    <p><a class="btn btn-main" href="#">Más info</a></p>
-  </div>
-</div>
+    echo slide_query($args);
+  ?>
 </section>
